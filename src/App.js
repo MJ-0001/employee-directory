194464618132ce3ebc4ad3140ec593dc912bf2eb
Matthew Jones <mejones92@gmail.com>
2021-05-07 12:45:50 +0100

    Initial commit of project files.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import Jumbotron from './components/Jumbotron';
+
+export default function App() {
+  return (
+    <>
+      <Jumbotron />
+    </>
+  );
+};
+
---
+++ src/components/Jumbotron/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+export default function Jumbotron () {
+  return (
+    <div>
+      <div className="jumbotron jumbotron-fluid bg-info">
+        <div className="container">
+          <h1 className="display-4 text-center">Employee Directory</h1>
+        </div>
+      </div>
+    </div>
+  )
+};
