6c9453280f279a9b99500e5795e392e51988d785
Matthew Jones <mejones92@gmail.com>
2021-05-08 16:18:22 +0100

    Main JS file for rendering React components to page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 // import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
-import Jumbotron from './components/Jumbotron';
+import Site from './components/pages/index'
 
 export default function App() {
   return (
     <>
-      <Jumbotron />
+      <Site />
     </>
   );
 };
