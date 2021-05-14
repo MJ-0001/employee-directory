b06316e484f860ae38e055a748a8ce7c8d146897
Matthew Jones <mejones92@gmail.com>
2021-05-14 23:55:27 +0100

    Added search for user functionality

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,39 +3,52 @@ import Jumbotron from '../components/Jumbotron';
 import Container from '../components/Container';
 import Row from '../components/Row';
 import Col from '../components/Col';
+import getUsers from '../utils/API';
 
 function Site() {
   const [users, setUsers] = useState([]);
-  const [search, setSearch] = useState('');
+  const [itemInput, setItemInput] = useState('');
 
   useEffect(() => {
-    async function fetchData() {
-      await fetch('https://randomuser.me/api/?results=4')
-      .then(res => res.json())
-      .then(data => {
-        setUsers(data.results);
-      })
-    }
-    fetchData();
-  }, []);
+    userData()
+  }, [])
 
-  useEffect(() => {
-    if (search !== '') {
-      async function fetchData() {
-        await fetch('https://randomuser.me/api/?name='+search)
-        .then(res => res.json())
-        .then(data => {
-          setSearch(data.results);
-        })
-      }
-      fetchData();
-    }
-  }, [search]);
-
-  const searchUsers = (event) => {
-    let value = JSON.stringify(event.target.value);
-    console.log(value)
+  function userData () {
+    getUsers()
+    .then(res => setUsers(res.data.results))
+    .catch(err => console.log(err));
   }
+
+  const handleInputChange = event => {
+    const { value } = event.target;
+    setItemInput(value);
+  }
+
+  const handleFormSubmit = event => {
+    event.preventDefault();
+
+    const name = itemInput;
+    console.log(name)
+    for (let i = 0; i < users.length; i++) {
+      if (Object.values(users[i].name).includes(name)) {
+        let newUser = [];
+        let person = users[i];
+        newUser.push(person);
+        console.log(newUser);
+        setUsers(newUser);
+        break;
+      }
+    }
+    
+
+    // document.getElementById('search').addEventListener('onClick', () => {
+    //   document.getElementsByTagName('input')[0].value = '';
+    // });
+  }
+
+  // function clearSearch() {
+  //   userData();
+  // }
 
   if (users) {
     return (
@@ -43,20 +56,20 @@ function Site() {
       <Jumbotron />
       <Container>
         <Row>
-          <Col size="3">
-            <input
-              className="form-control row-centered mt-3 mb-3 d-inline"
-              type="text"
-              placeholder="Search"
-              name="username"
-              onChange={searchUsers}
-            />
+          <Col size="6">
+            <div className="w-50">
+              <form>
+                <input
+                  className="search form-control row-centered mt-3 mb-2 d-inline-block"
+                  type="text"
+                  placeholder="Search"
+                  name="ItemInput"
+                  onChange={handleInputChange} value={itemInput}
+                  />
+                <button className="btn btn-success mb-2" type="submit" onClick={handleFormSubmit}>Submit</button>
+              </form>
+            </div>
           </Col>
-          {/* <Col size="2">
-            <button className="btn btn-success mt-3" type="submit" onClick={searchUsers}>
-              Submit
-            </button>
-          </Col> */}
         </Row>
         <Row className="form-group border border-info rounded">
           <Col size="12">
@@ -89,10 +102,7 @@ function Site() {
       </Container>
     </div>
     )
-  } else {
-    return <h1>Unable to load data</h1>
   }
 } 
-
 
 export default Site;
