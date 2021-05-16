193566660b5d8e51e34fe3c9e81294e4464d846e
Matthew Jones <mejones92@gmail.com>
2021-05-16 13:04:48 +0100

    Added import for style.css.

--- src/components/Jumbotron/index.jsx
+++ src/components/Jumbotron/index.jsx
@@ -7,7 +7,7 @@ export default function Jumbotron () {
       <div className="jumbotron jumbotron-fluid">
         <div className="container text-center">
           <h1 className="display-4 ">Employee Directory</h1>
-          <p className="p-header">Click headings to filter or use search box to narrow results</p>
+          <p className="p-header">Filter by name or use search box to narrow results</p>
         </div>
       </div>
     </>
