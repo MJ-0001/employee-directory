067062e6c2c6414f05cff6c5f9633aeb0cae9de8
Matthew Jones <mejones92@gmail.com>
2021-05-16 13:06:06 +0100

    Added useEffect functions to handle state change when table of users is sorted or a search is made.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,105 +4,97 @@ import Container from '../components/Container';
 import Row from '../components/Row';
 import Col from '../components/Col';
 import getUsers from '../utils/API';
+import '../index.css';
 
-function Site() {
-  const [users, setUsers] = useState([]);
+const Site = () => {
   const [itemInput, setItemInput] = useState('');
+  const [users, setUsers] = useState([]);
+  const [isSorted, setIsSorted] = useState(false);
 
   useEffect(() => {
-    userData()
-  }, [])
-
-  function userData () {
     getUsers()
     .then(res => setUsers(res.data.results))
     .catch(err => console.log(err));
-  }
-
-  const handleInputChange = event => {
-    const { value } = event.target;
-    setItemInput(value);
-  }
-
-  const handleFormSubmit = event => {
-    event.preventDefault();
-
+  }, [])
+  
+  useEffect(() => {
     const name = itemInput;
-    console.log(name)
+    
     for (let i = 0; i < users.length; i++) {
       if (Object.values(users[i].name).includes(name)) {
         let newUser = [];
         let person = users[i];
         newUser.push(person);
-        console.log(newUser);
         setUsers(newUser);
         break;
       }
     }
-    
+  }, [itemInput])
 
-    // document.getElementById('search').addEventListener('onClick', () => {
-    //   document.getElementsByTagName('input')[0].value = '';
-    // });
+  useEffect(() => {
+      document.getElementById('sort-name').addEventListener('click', () => {
+        setUsers(users.sort((a, b) => (a.name.first > b.name.first) ? 1 : -1))
+      })
+  }, [users])
+
+  const resetNames = (e) => {
+    e.preventDefault();
+    window.location.reload();
   }
-
-  // function clearSearch() {
-  //   userData();
-  // }
-
-  if (users) {
-    return (
-    <div>
-      <Jumbotron />
-      <Container>
-        <Row>
-          <Col size="6">
-            <div className="w-50">
-              <form>
-                <input
-                  className="search form-control row-centered mt-3 mb-2 d-inline-block"
-                  type="text"
-                  placeholder="Search"
-                  name="ItemInput"
-                  onChange={handleInputChange} value={itemInput}
-                  />
-                <button className="btn btn-success mb-2" type="submit" onClick={handleFormSubmit}>Submit</button>
-              </form>
-            </div>
-          </Col>
-        </Row>
-        <Row className="form-group border border-info rounded">
-          <Col size="12">
-          <table className="table table-striped text-center mb-0">
-            <thead>
-              <tr>
-                <th scope="col">Image</th>
-                <th scope="col">Name</th>
-                <th scope="col">Phone</th>
-                <th scope="col">Email</th>
-                <th scope="col">DoB</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user, index) => {
-                return (      
-                  <tr key={index}>
-                    <td><img src={user.picture.thumbnail} alt={user.name.first + user.name.last + ""}></img></td>
-                    <td>{user.name.first + ' ' + user.name.last}</td>
-                    <td>{user.phone}</td>
-                    <td>{user.email}</td>
-                    <td>{user.dob.date.substring(0, 10)}</td> 
-                </tr> 
-                )
-                })}
-            </tbody>
-          </table>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-    )
-  }
+  
+  return (
+  <div>
+    <Jumbotron />
+    <Container>
+      <Row>
+        <Col size="6">
+          <div className="d-inline-block">
+            <form className="search d-inline-block search-form me-4">
+              <input
+                id="search-box"
+                className="form-control row-centered mt-3 mb-2 d-inline-block"
+                type="text"
+                placeholder="Search"
+                name="ItemInput"
+                onChange={event => setItemInput(event.target.value)} 
+                value={itemInput}
+                />
+            </form>
+            <button className="d-inline-block btn btn-success me-4 my-button" onClick={resetNames}>Reset</button>
+          </div>
+        </Col>
+      </Row>
+      <Row className="form-group my-border rounded mb-5">
+        <Col size="12">
+        <table className="table table-striped text-center mb-0">
+          <thead>
+            <tr>
+              <th scope="col">Image</th>
+              <th scope="col"><button id="sort-name" className="table-btn">Name</button></th>
+              <th scope="col">Phone</th>
+              <th scope="col">Email</th>
+              <th scope="col">DoB</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => {
+              return (      
+                <tr key={index}>
+                  <td><img src={user.picture.thumbnail} alt={user.name.first + user.name.last + ""}></img></td>
+                  <td>{user.name.first + ' ' + user.name.last}</td>
+                  <td>{user.phone}</td>
+                  <td>{user.email}</td>
+                  <td>{user.dob.date.substring(0, 10)}</td> 
+              </tr> 
+              )
+              })}
+          </tbody>
+        </table>
+        </Col>
+      </Row>
+    </Container>
+  </div>
+  )
 } 
 
 export default Site;
